a74cbd27106eaacad20a176e79ff80cd59c65ad8
Sean Gibbons <gibbonss@bishopireton.org>
2017-10-25 14:07:16 -0400

    dab

---
+++ StockExchange/src/SafeTradeTest.java
@@ -0,0 +1,10 @@
+
+public class SafeTradeTest {
+
+	public static void main(String[] args) {
+		
+		Stock s = new Stock("HELPME", "Tutoring, Inc.", 1000.50);
+		System.out.println(s);
+	}
+
+}
---
+++ StockExchange/src/Stock.java
@@ -0,0 +1,60 @@
+import java.text.DecimalFormat;
+import java.util.PriorityQueue;
+
+public class Stock implements Comparable<Stock>{
+
+	public static DecimalFormat money = new DecimalFormat("$#,##0.00");
+	
+	private String symbol;
+	private String name;
+	private double lowPrice, lastPrice, highPrice;
+	private PriorityQueue<TradeOrder> buy;
+	private PriorityQueue<TradeOrder> sell;
+
+	
+	
+	public Stock(String s, String n, double p) {
+		symbol = s;
+		name = n;
+		lowPrice = p;
+		lastPrice = p;
+		highPrice = p;
+
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getLastPrice() {
+		return lastPrice;
+	}
+
+	public double getLowPrice() {
+		return lowPrice;
+	}
+
+	public double getHighPrice() {
+		return highPrice;
+	}
+
+
+	public DecimalFormat getMoney() {
+		return money;
+	}
+
+	@Override
+	public String toString() {
+		return getSymbol() + " " + getName() + " " + getMoney().format(getPrice());
+	}
+
+	@Override
+	public int compareTo(Stock arg0) {
+		return (int) getLastPrice()-s.getLastPrice();
+	}
+
+}
